d6fbcea859ecb7d5afd3cecb3ba9e3133df42132
majorl3oat <pongrapee_a>
2020-02-10 21:04:03 +0530

    -Add Storyboard Elements -Simply Navigation -Add setting icon -Add setting viewcontroller

---
+++ hiragana/SettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingViewController.swift
+//  hiragana
+//
+//  Created by Pongrapee Attasaranya on 2020/02/10.
+//  Copyright © 2020 Majorl3oat. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBAction func saveSetting(sender: UIButton) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
